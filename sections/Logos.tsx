2dfb23681831ccfff381594560387eee3908ac0f
Manu <manu@deco.cx>
2024-08-22 09:48:37 -0300

    adami ads

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -29,8 +29,18 @@ export default function Logos({
           <Image
             src={logo.src || ""}
             alt={logo.altText || ""}
-            width={110}
-            height={25}
+            width={130}
+            height={60}
+          />
+        );
+      })}
+       {logos?.map((logo) => {
+        return (
+          <Image
+            src={logo.src || ""}
+            alt={logo.altText || ""}
+            width={130}
+            height={60}
           />
         );
       })}
@@ -39,9 +49,9 @@ export default function Logos({
   return (
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-12">
-        <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+        <p class="text-center text-3xl font-bold">{title}</p>
+        <div class="relative w-full overflow-hidden h-20">
+          <div class="animate-marquee absolute top-0 left-0 flex flex-nowrap h-16">
             {slideContent}
           </div>
         </div>
